ac85720b930ef5a5d2ea70b2156f6a4f18191224
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-16 13:41:32 -0300

    Aplicando o type no input

--- cypress/e2e/ebac-agenda/home.cy.js
+++ cypress/e2e/ebac-agenda/home.cy.js
@@ -2,6 +2,7 @@
 
 describe('Teste para agenda-contatos', () => {
     it('Deve incluir um novo contato', () => {
-        cy.get('sc-gLDzan ckeKmo input').type
+        cy.get('sc-gLDzan ckeKmo input').type('Maria')
+        
     })
 })
