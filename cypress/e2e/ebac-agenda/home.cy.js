a223009cc73e19bca6fed5467f1d3af57e293670
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-17 10:49:45 -0300

    Push do URL dentro do visit

--- cypress/e2e/ebac-agenda/home.cy.js
+++ cypress/e2e/ebac-agenda/home.cy.js
@@ -2,7 +2,7 @@
 
 describe('Teste para agenda-contatos', () => {
     it('Deve incluir um novo contato', () => {
-        cy.visit('')
+        cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
         cy.get('sc-gLDzan ckeKmo input').
 
     })
