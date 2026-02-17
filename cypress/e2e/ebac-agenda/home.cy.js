b32f0f8d392141e1dce722eb7f9605b3cfd7fad7
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-17 11:36:32 -0300

    Aplicando o beforeEach

--- cypress/e2e/ebac-agenda/home.cy.js
+++ cypress/e2e/ebac-agenda/home.cy.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Teste para agenda-contatos', () => {
-    it('Deve adicionar um novo contato', () => {
+    beforeEach(() => {
         cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
+    })
+
+
+    it('Deve adicionar um novo contato', () => {
+        
         cy.get('input[type="text"]').type('João da Silva')
         cy.get('input[type="email"]').type('joao@exemplo.com')
         cy.get('input[type="tel"]').type('11987654321')
