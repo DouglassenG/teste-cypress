fafd957c8dc4ade03c7e7a118f990df039ea08ae
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-17 11:44:20 -0300

    Aplicando as edição e exclusão no teste

--- cypress/e2e/ebac-agenda/home.cy.js
+++ cypress/e2e/ebac-agenda/home.cy.js
@@ -5,12 +5,18 @@ describe('Teste para agenda-contatos', () => {
         cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
     })
 
-
-    it('Deve adicionar um novo contato', () => {
-        
+    it('Deve adicionar um novo contato', () => {        
         cy.get('input[type="text"]').type('João da Silva')
         cy.get('input[type="email"]').type('joao@exemplo.com')
         cy.get('input[type="tel"]').type('11987654321')
         cy.get('button[type="submit"]').click()
+    })
+    it('Deve editar um contato existente', () => {
+        cy.get(':nth-child(5) > .sc-gueYoa > .edit').click()
+        cy.get('input[type="text"]').clear().type('Maria Silva')
+        cy.get('.alterar').click()
+    })
+    it('Deve excluir um contato', () => {
+        cy.get(':nth-child(5) > .sc-gueYoa > .delete').click()
     })
 })
