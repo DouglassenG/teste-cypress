2ef1d7726b1e69edba786e03bc476aab3d70adcf
Douglas <douglasmsrodrigues@hotmail.com>
2026-02-17 11:25:58 -0300

    Aplicando o get nos inputs

--- cypress/e2e/ebac-agenda/home.cy.js
+++ cypress/e2e/ebac-agenda/home.cy.js
@@ -4,5 +4,7 @@ describe('Teste para agenda-contatos', () => {
     it('Deve adicionar um novo contato', () => {
         cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
         cy.get('input[type="text"]').type('João da Silva')
+        cy.get('input[type="email"]').type('joao@exemplo.com')
+        cy.get('input[type="tel"]').type('11987654321')
     })
 })
